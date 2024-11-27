ac3bca37b99137ae4a242a3be622151f1c25f801
Antonio Ortiz <ortiz.antonio@gmail.com>
2024-11-27 00:32:25 -0600

    added postcss and running only on prod

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: [
     require('postcss-import-ext-glob'),
     require('postcss-import'),
-    require('tailwindcss')
+    require('tailwindcss'),
+    ...(process.env.NODE_ENV === 'prod'
+        ? [require("cssnano")({ preset: "default" })]
+        : [])
   ]
 };
